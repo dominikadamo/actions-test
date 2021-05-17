77d5c82c3ae442498646eecd60157ea2ad03c7aa
Dominik Adamowski <do1adamo@gmail.com>
2021-05-17 04:37:01 +0200

    Version

--- packages/webapp/src/App.js
+++ packages/webapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          6th version
+          7th version
         </p>
       </header>
     </div>
