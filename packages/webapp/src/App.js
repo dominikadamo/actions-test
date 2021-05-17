24d00d19c86baf0f33a9953b032ee863a0581628
Dominik Adamowski <do1adamo@gmail.com>
2021-05-17 04:26:16 +0200

    Update deploy.yml

--- packages/webapp/src/App.js
+++ packages/webapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          5th version
+          6th version
         </p>
       </header>
     </div>
